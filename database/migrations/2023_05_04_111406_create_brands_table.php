7ec039005cc24fe7992e15af7c49ebb52d6221b0
Muhammad Reda <muhammadreda@arabyads.com>
2023-05-07 18:10:22 +0300

    change to migration

--- database/migrations/2023_05_04_111406_create_brands_table.php
+++ database/migrations/2023_05_04_111406_create_brands_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('name');
             $table->string('image');
             $table->timestamps();
         });
